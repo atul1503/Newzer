cdf15994edd67819c9ec710a6d951cc817ff48ad
atul1503 <atultripathi589@gmail.com>
2021-06-10 23:16:39 +0530

    autoCommit

--- HelloDjango/models.py
+++ HelloDjango/models.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class preference(forms.Form):
-    body=forms.CharField(label='Search in article body',max_length=30)
+    body=forms.CharField(label='Search in article body',max_length=30,required=False)
     title=forms.CharField(label='Search in article title',max_length=30,required=False)
     max_articles=forms.IntegerField(label='Number of Articles',initial=10)
     
