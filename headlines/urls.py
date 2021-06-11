7aa7a9c48e86b7840138df03887cdb61e0b021c1
atul1503 <atultripathi589@gmail.com>
2021-06-11 21:14:47 +0530

    autoCommit

--- headlines/urls.py
+++ headlines/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns=[
     path('',general),# '' is /top_headlines/
-    re_path('general?prev=.*',gen_prev)
+    re_path('general/prev?.*',gen_prev),
+    re_path('general/next?.*',gen_next)
     
 ]
--- headlines/views.py
+++ headlines/views.py
@@ -20,10 +20,38 @@ def general(request):
     ])
     r=requests.get(url)
     obj=json.loads(r.text)
-    print(obj['articles'][0])
     return render(request,'headlines.html',{'articles':obj['articles'],'page':1})
     
-def gen_prev(request)
+def gen_prev(request):
+    '''
+    general form back view
+    '''
+    page=int(request.GET.get('page'))
+    url=url_maker('https://newsapi.org/v2/top-headlines?',
+    [
+     ['category','general'],
+     ['page',page-1],
+     ['apiKey',api_key],
+    ])
+    r=requests.get(url)
+    obj=json.loads(r.text)
+    return render(request,'headlines.html',{'articles':obj['articles'],'page':page-1})
+    
+def gen_next(request):
+    '''
+    general form front view
+    '''
+    page=int(request.GET.get('page'))
+    url=url_maker('https://newsapi.org/v2/top-headlines?',
+    [
+     ['category','general'],
+     ['page',page+1],
+     ['apiKey',api_key],
+    ])
+    r=requests.get(url)
+    obj=json.loads(r.text)
+    return render(request,'headlines.html',{'articles':obj['articles'],'page':page+1})
+    
     
     
     
