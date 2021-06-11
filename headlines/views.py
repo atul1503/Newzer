5d5de0f4f89b5b360a45221f3f2bcc148a7257a2
atul1503 <atultripathi589@gmail.com>
2021-06-11 16:40:58 +0530

    autoCommit

--- headlines/views.py
+++ headlines/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from HelloDjango.utility import *
+import json
+import requests
 
 # Create your views here.
+
+def general(request):
+    '''
+    Get initial News from general category.
+    '''
+    url=url_maker('https://newsapi.org/v2/top-headlines?',{
+    'category':'general'
+    'page':1
+    }
+    r=requests.get(url)
+    obj=json.loads(r.text)
+    return render(request,'headlines.html',{'articles':json['articles'],'page':1}
+    
+    
+    
+    
+    
+    
+    
+    
