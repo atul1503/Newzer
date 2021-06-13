332cb818b51b4d37903da27bfcbcfd30d9df3400
atul1503 <atultripathi589@gmail.com>
2021-06-13 18:19:27 +0530

    autoCommit

--- headlines/views.py
+++ headlines/views.py
@@ -12,6 +12,10 @@ def general(request):
     '''
     Get top headlines.
     '''
+    if 'next' in request.GET:
+        return gen_next(request)
+    elif 'prev' in request.GET:
+        return gen_prev(request)
     category=request.GET.get('categ',0)
     if not category:
         category='general'
@@ -21,6 +25,8 @@ def general(request):
      ['page',1],
      ['apiKey',api_key],
     ])
+    request.session['pref']=request.GET
+    request.session['page']=1
     r=requests.get(url)
     obj=json.loads(r.text)
     return render(request,'headlines.html',{'articles':obj['articles'],'page':1})
@@ -29,36 +35,38 @@ def gen_prev(request):
     '''
     headlines form back view
     '''
-    category=request.GET.get('categ',0)
+    category=request.session['pref']['categ']
+    page=int(request.session['page'])-1
     if not category:
         category='general'
-    page=int(request.GET.get('pageno'))
     url=url_maker('https://newsapi.org/v2/top-headlines?',
     [
-     ['category','general'],
-     ['page',page-1],
+     ['category',category],
+     ['page',page],
      ['apiKey',api_key],
     ])
     r=requests.get(url)
     obj=json.loads(r.text)
+    request.session['page']=page
     return render(request,'headlines.html',{'articles':obj['articles'],'page':page-1})
     
 def gen_next(request):
     '''
     headlines form front view
     '''
-    category=request.GET.get('categ',0)
+    category=request.session['pref']['categ']
     if not category:
         category='general'
-    page=int(request.GET.get('pageno'))
+    page=int(request.session['page'])+1
     url=url_maker('https://newsapi.org/v2/top-headlines?',
     [
-     ['category','general'],
-     ['page',page+1],
+     ['category',category],
+     ['page',page],
      ['apiKey',api_key],
     ])
     r=requests.get(url)
     obj=json.loads(r.text)
+    request.session['page']=page
     return render(request,'headlines.html',{'articles':obj['articles'],'page':page+1})
     
     
