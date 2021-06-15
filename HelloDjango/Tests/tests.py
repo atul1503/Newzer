f2b810748ffdf9a89490d7e79af3be3c1ba8bed4
atul1503 <atultripathi589@gmail.com>
2021-06-15 13:43:57 +0530

    autoCommit

--- HelloDjango/Tests/tests.py
+++ HelloDjango/Tests/tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
-
+import jsonpickle
     
 
 
 class NewsViewTest(TestCase):
     def test_forHomePage(self):
         response=self.client.get('/news')
-        
         self.assertEqual(response.status_code,200)
         response=self.client.get('/news',{'next':''})
         self.assertEqual(response.status_code,200)
@@ -15,7 +14,16 @@ class NewsViewTest(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertGreaterEqual(len(response.context['articles']),1)
         
-
+    def test2(self):
+        response=self.client.get('/news',{'q':'Stack Overflow'})
+        self.assertEqual(response.status_code,200)
+        
+    def test3(self):
+        self.client.get('/news')
+        prev_pref_body=jsonpickle.decode(self.client.session['pref'])['body']
+        self.client.get('/news',{'next':''})
+        self.assertEqual(jsonpickle.decode(self.client.session['pref'])['body'],prev_pref_body)
+        
         
 
         
