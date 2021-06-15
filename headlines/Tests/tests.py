149280c82104b1b1f398dd1a8139e72da2b9b65a
atul1503 <atultripathi589@gmail.com>
2021-06-15 13:12:50 +0530

    autoCommit

--- headlines/Tests/tests.py
+++ headlines/Tests/tests.py
@@ -6,6 +6,20 @@ class headliner(TestCase):
         response=self.client.get('/top_headlines')
         self.assertEqual(response.status_code,200)
         prev_categ=self.client.session['pref']['categ']
-        response=self.client.get('/top_headlines',{'categ':'health'})
+        self.client.get('/top_headlines',{'categ':'health'})
         self.assertNotEqual(self.client.session['pref']['categ'],prev_categ)
+    
+    def test1(self):
+        response=self.client.get('/top_headlines')
+        self.assertEqual(response.status_code,200)
+        prev_categ=self.client.session['pref']['categ']
+        self.client.get('/top_headlines',{'next':''})
+        self.assertEqual(self.client.session['pref']['categ'],prev_categ)
+        
+    def test2(self):
+        response=self.client.get('/top_headlines',{'categ':'health'})
+        self.client.get('/top_headlines',{'next':''})
+        self.client.get('/top_headlines',{'next':''})
+        self.client.get('/top_headlines',{'next':''})
+        self.assertEqual(self.client.session['pref']['categ'],'health')
         
